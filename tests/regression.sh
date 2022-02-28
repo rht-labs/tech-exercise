54faee83cdf8bb59fea798cfe2fced3a2900acc1
Mike Hepburn <eformat@gmail.com>
2022-02-28 17:22:55 +1000

    ✪ testing experiments ✪

--- tests/regression.sh
+++ tests/regression.sh
@@ -72,6 +72,37 @@ EOF
     fi
 }
 
+gitlab_setup() {
+    echo "Setting up Gitlab ..."
+    # get csrf from login page
+    gitlab_basic_auth_string="Basic $(echo -n ${GITLAB_USER}:${GITLAB_PASSWORD} | base64)"
+    body_header=$(curl -L -s -H "Authorization: ${gitlab_basic_auth_string}" -c cookies.txt -i "${GIT_SERVER}/users/sign_in")
+    csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /new_user.*?authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
+    # login
+    curl -s -H "Authorization: ${gitlab_basic_auth_string}" -b cookies.txt -c cookies.txt -i "https://${GIT_SERVER}/users/auth/ldapmain/callback" \
+                        --data "username=${GITLAB_USER}&password=${GITLAB_PASSWORD}" \
+                        --data-urlencode "authenticity_token=${csrf_token}" \
+                        > /dev/null
+    # generate personal access token form
+    body_header=$(curl -L -H "Authorization: ${gitlab_basic_auth_string}" -H 'user-agent: curl' -b cookies.txt -i "https://${GITLAB_SERVER}/profile/personal_access_tokens" -s)
+    csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
+    # ccrape the personal access token from the response
+    body_header=$(curl -s -L -H "Authorization: ${gitlab_basic_auth_string}" -b cookies.txt "https://${GIT_SERVER}/profile/personal_access_tokens" \
+                        --data-urlencode "authenticity_token=${csrf_token}" \
+                        --data 'personal_access_token[name]='"${GITLAB_USER}"'&personal_access_token[expires_at]=&personal_access_token[scopes][]=api')
+    personal_access_token=$(echo $body_header | perl -ne 'print "$1\n" if /created-personal-access-token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
+    # get or create group id
+    group_id=$(curl -s -k -L -H "Accept: application/json" -H "PRIVATE-TOKEN: ${personal_access_token}" -X GET "https://${GIT_SERVER}/api/v4/groups?search=${groupname}" | jq -c '.[] | .id')
+    if [ -z $group_id ]; then
+        group_id=$(curl -s -k -L -H "Accept: application/json" -H "PRIVATE-TOKEN: ${personal_access_token}" -X POST "https://${GIT_SERVER}/api/v4/groups" --data "name=${groupname}&path=${groupname}&visibility=public" | jq -c '.id')
+    fi
+    # delete team project
+    curl -s -k -H "PRIVATE-TOKEN: ${personal_access_token}" -X DELETE "https://${GIT_SERVER}/api/v4/projects/${TEAM_NAME}%2Ftech-exercise" >/dev/null 2>&1
+    # create project
+    curl -s -k -H "PRIVATE-TOKEN: ${personal_access_token}" -X POST "https://${GIT_SERVER}/api/v4/projects" --data "name=tech-exercise&visibility=public&namespace_id=${group_id}" > /dev/null 2>&1
+}
+
+
 test_file() {
     local file=$1
     local tags=$2
@@ -142,6 +173,7 @@ setup_tests() {
     patch_rundoc
     git_checkout
     perform_logins
+    gitlab_setup
 }
 
 test_the_manual_menance() {
