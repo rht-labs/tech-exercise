f58e9dd7c442edffc7098e91c802d61e0a181590
Mike Hepburn <eformat@gmail.com>
2022-03-03 15:51:53 +1000

    ✪ testing sanitize output ✪

--- tests/regression.sh
+++ tests/regression.sh
@@ -87,18 +87,18 @@ gitlab_setup() {
     echo "Setting up Gitlab ..."
     # get csrf from login page
     gitlab_basic_auth_string="Basic $(echo -n ${GITLAB_USER}:${GITLAB_PASSWORD} | base64)"
-    body_header=$(curl -L -s -H "Authorization: ${gitlab_basic_auth_string}" -c cookies.txt -i "https://${GIT_SERVER}/users/sign_in")
+    body_header=$(curl -L -s -H "Authorization: ${gitlab_basic_auth_string}" -c /tmp/cookies.txt -i "https://${GIT_SERVER}/users/sign_in")
     csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /new_user.*?authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
     # login
-    curl -s -H "Authorization: ${gitlab_basic_auth_string}" -b cookies.txt -c cookies.txt -i "https://${GIT_SERVER}/users/auth/ldapmain/callback" \
+    curl -s -H "Authorization: ${gitlab_basic_auth_string}" -b /tmp/cookies.txt -c /tmp/cookies.txt -i "https://${GIT_SERVER}/users/auth/ldapmain/callback" \
                         --data "username=${GITLAB_USER}&password=${GITLAB_PASSWORD}" \
                         --data-urlencode "authenticity_token=${csrf_token}" \
                         > /dev/null
     # generate personal access token form
-    body_header=$(curl -L -H "Authorization: ${gitlab_basic_auth_string}" -H 'user-agent: curl' -b cookies.txt -i "https://${GIT_SERVER}/profile/personal_access_tokens" -s)
+    body_header=$(curl -L -H "Authorization: ${gitlab_basic_auth_string}" -H 'user-agent: curl' -b /tmp/cookies.txt -i "https://${GIT_SERVER}/profile/personal_access_tokens" -s)
     csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
     # ccrape the personal access token from the response
-    body_header=$(curl -s -L -H "Authorization: ${gitlab_basic_auth_string}" -b cookies.txt "https://${GIT_SERVER}/profile/personal_access_tokens" \
+    body_header=$(curl -s -L -H "Authorization: ${gitlab_basic_auth_string}" -b /tmp/cookies.txt "https://${GIT_SERVER}/profile/personal_access_tokens" \
                         --data-urlencode "authenticity_token=${csrf_token}" \
                         --data 'personal_access_token[name]='"${GITLAB_USER}"'&personal_access_token[expires_at]=&personal_access_token[scopes][]=api')
     personal_access_token=$(echo $body_header | perl -ne 'print "$1\n" if /created-personal-access-token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
