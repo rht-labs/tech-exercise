3255d47cb2dddea01566c8fd950280a784c5978f
Mike Hepburn <eformat@gmail.com>
2022-03-11 10:22:27 +1000

    🪓 user group wrong 🪓

--- tests/regression.sh
+++ tests/regression.sh
@@ -224,6 +224,8 @@ setup_tests() {
     if [[ ! -z ${GITSETUP} && -z ${skipgitlab} ]]; then
         gitlab_setup
     fi
+    # for testing cluster
+    fix_student_role
 }
 
 test_the_manual_menance() {
