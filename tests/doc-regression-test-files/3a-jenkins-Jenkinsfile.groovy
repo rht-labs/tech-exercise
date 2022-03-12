796c8b3d4c975d818e32df5f80c2fab011679b7c
Mike Hepburn <eformat@gmail.com>
2022-03-13 09:57:24 +1000

    ✪ testing - tekton ✪

--- tests/doc-regression-test-files/3a-jenkins-Jenkinsfile.groovy
+++ tests/doc-regression-test-files/3a-jenkins-Jenkinsfile.groovy
@@ -216,7 +216,7 @@ pipeline {
 						sh  '''
 							git clone https://${GIT_CREDS}@${ARGOCD_CONFIG_REPO} config-repo
 							cd config-repo
-							git checkout ${ARGOCD_CONFIG_REPO_BRANCH} # master or main
+							git checkout -b ${ARGOCD_CONFIG_REPO_BRANCH} --track origin/${ARGOCD_CONFIG_REPO_BRANCH} # master or main
 				
 							PREVIOUS_VERSION=$(yq eval .applications.\\"${APP_NAME}\\".values.image_version "${ARGOCD_CONFIG_REPO_PATH}")
 							PREVIOUS_CHART_VERSION=$(yq eval .applications.\\"${APP_NAME}\\".source_ref "${ARGOCD_CONFIG_REPO_PATH}")
