e041c0defa5ad88b34a46b244cf7ec6cf24038bd
Cansu <31902747+ckavili@users.noreply.github.com>
2022-08-05 13:28:55 +0300

    🌱typo fix🌱

--- tests/container/run.sh
+++ tests/container/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# capture en vars, othwerwise use default values
+# capture en vars, otherwise use default values
 TECH_EXERCISE_REPO="${REPO:=https://github.com/rht-labs/tech-exercise.git}"
 TECH_EXERCISE_BRANCH="${BRANCH:=main}"
 
